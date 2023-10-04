03423e3992e45705883c32a42c7feea2fd1a665e
frangarayco <143758169+frangarayco@users.noreply.github.com>
2023-10-04 18:53:33 -0300

    Add files via upload

--- lagrange.py
+++ lagrange.py
@@ -1,26 +1,40 @@
-from math import * 
-
-def lagrange (datos): 
-    def L(k, x):
-        out = 1
-        for i, p in enumerate(datos):
-            if (i != k):
-                out *= (x-p[0])/(datos[k][0]-p[0])
-        return out
-    
-    def P(x): 
-        lag= 0
-        for k, p in enumerate(datos):
-            lag += p[1]*L(k,x)
-        return lag
-    
-    return P
-
-#datos = [(2.0, 1.0/2.0), (11.0/4.0, 4.0/11.0), (4.0, 1.0/4.0)]
-#Pf = lagrange(datos)
-#print (Pf(3))
+import numpy as np 
+from sympy import *
 
 
-datos =[(1.0, 0.1411), (1.3, -0.6878), (1.6, -0.9962), (1.9, -0.5507), (2.2, 0.3115)]
-pf = lagrange(datos)
-print(pf(1.5))
+def polL(xi,fi): 
+    x = Symbol("x")
+    n = len(xi)
+    pol=0
+    for i in range(n):
+        numerador=1
+        denominador=1
+        for j in range(n):
+            if j != i:
+                numerador *=(x-xi[j])
+                denominador *= (xi[i]-xi[j])
+        termino=numerador/denominador 
+        pol+=termino*fi[i]  
+    return pol  
+
+
+def polL_eval(xi,fi,a): 
+    resu=0
+    for i in range(len(xi)):
+        numerador=1
+        denominador=1
+        for j in range(len(xi)):
+            if j != i:
+                numerador *=(a-xi[j]) #aca reemplazo a, que es el valor donde quiero evaluar el polinomio de lagrange
+                denominador *= (xi[i]-xi[j])
+        termino=numerador/denominador 
+        resu+=termino*fi[i]  
+    return resu  
+        
+
+
+xi= np.array([1.0,1.3,1.6,1.9,2.2])
+fi= np.array([0.1411,-0.6878,-0.9962,-0.5507,0.3115])
+a = 1.5
+print(polL(xi,fi))
+print("El valor de polinomio evaluado en {a} es:", polL_eval(xi,fi,a))
