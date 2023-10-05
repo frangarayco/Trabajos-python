d0818b3861e351fc98d1efc880d58bfacf653eba
frangarayco <143758169+frangarayco@users.noreply.github.com>
2023-10-05 16:39:06 -0300

    .

--- puntofijo.py
+++ puntofijo.py
@@ -1,19 +1,38 @@
 from math import *
+from tabulate import *
 
-def puntofijo (f,p0,error,n):
-    i=1
-    while (i <= n):
+
+mat = []
+
+
+def cargarMat(p,p0,i,f):
+    fila =[i,p0,p,f(p)]
+    mat.append(fila)
+    return
+
+def puntofijo (f,p0,tol,iterMax):
+    i = 0
+    while i < iterMax : 
         p = f(p0)
-        print (" Iter = " , " %03d" % i, " ; p = " , " %.14f" % p)
-        if (abs(p-p0) < error):
-            return p
+        if (abs(p-p0) < tol):
+            break
+        cargarMat(p,p0,i,f)
         p0 = p
         i += 1
-    print ("Iteraciones terminadas: error")
-    return  
+
+        
+    headers = ["   iteraciones      ", "         Valor p0:         ","       Valor de p          ","             Valor de f(p):         "]
+    tabla = tabulate(mat, headers=headers, tablefmt="grid")
+    print(tabla)
+
+
+    return p
+      
+    
+      
 
 
 f = lambda x: (x**2-1) /3
-puntofijo(f,0.9, 1e-10,100)
+puntofijo(f,0.9, 1e-10,10)
 
 
