2ae64f0763ccc6a0e404fd4fe5e2461e56baa670
frangarayco <143758169+frangarayco@users.noreply.github.com>
2023-11-24 12:05:55 -0300

    Add files via upload

--- trapecio.py
+++ trapecio.py
@@ -1,9 +1,14 @@
 
 
-def trapecio (f,a,b):
-    return ((b-a)*(f(a)+f(b)))/2
+def trapecio (f,a,b,n):
+    h = (b-a)/n 
 
-
+    suma = 0.5 * (f(a)+f(b))
+    for i in range(1, n): 
+        suma += f(a+i*h)
+    resu = h*suma
+    return resu 
+        
 f = lambda x: x**3-6*x**2+11*x-6
 
-print(trapecio(f,1.3,1.8))
+print(trapecio(f,1.3,1.8,10))
